fff6f37de9b5db1ec8f0bcf838625172ac9dcb61
amjadkhanniazi <amjadkhanniazi010@gmail.com>
2024-05-06 15:15:18 +0500

    Section3 done for standard Laptop screen, Responsiveness yet to be added

--- src/components/sections/Section3.js
+++ src/components/sections/Section3.js
@@ -7,10 +7,15 @@ const Section3 = () => {
         <img src="https://static.vecteezy.com/system/resources/previews/007/081/882/original/people-working-in-a-table-meeting-and-the-co-working-shared-space-office-teamwork-business-collaboration-and-discussion-flat-style-illustration-vector.jpg" alt="Img of People Collaborating" />
       </div>
       <div className='Section3Div2'>
-        <h2>We Offer a Full Range of
-Digital Marketing Services!</h2>
+        <h2>We Offer a Full Range of 
+<u> Digital Marketing</u> Services!</h2>
         <p>Greatly explain attempt perhaps in feeling he. House men taste bed not drawn joy. Through enquire however do equally herself at. Greatly way old may you present improve. Wishing the feeling village him musical.</p>
-        
+        <ul class="services-list">
+        <li><strong>Global Reach</strong></li>
+        <li><strong>Big Experience</strong></li>
+        <li><strong>Convenience</strong></li>
+        <li><strong>Team Strength</strong></li>
+      </ul>
       </div>
     </div>
   );
